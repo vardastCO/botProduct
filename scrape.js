01de8962882d2ad0d45634ef8c9be7e7788f4c48
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-10-19 20:48:39 +0330

    c

--- scrape.js
+++ scrape.js
@@ -2,11 +2,9 @@ const { Cluster } = require('puppeteer-cluster');
 const { Client } = require('pg');
 const cron = require('node-cron');
 const fetch = require('node-fetch');
+const fs = require('fs-extra');
 const { v4: uuidv4 } = require('uuid');
 const Minio = require('minio');
-const puppeteer = require('puppeteer-core');
-
-
 const minioClient = new Minio.Client({
   endPoint: 'storage', // Use the service name defined in your Docker Compose file
   port: 9000,
@@ -48,106 +46,36 @@ const startUrlPattern2 = 'https://kashiland.com/store/prod';
 
 async function processPage(page) {
   try {
-    const pageUrl = page.url();
+    const pageUrl = await page.url();
     const uuid1 = uuidv4();
-    const [priceElement, nameElement, brandElement, categoryElemt] = await Promise.all([
+    const [priceElement, nameElement, brandElement, categoryElement] = await Promise.all([
       page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[1]/span[2]'),
       page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[1]/div/div/h1'),
       page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/ul/li[2]/a'),
       page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a'),
     ]);
+
     const priceText = priceElement.length > 0 ? await page.evaluate((el) => el.textContent, priceElement[0]) : '';
     const nameText = nameElement.length > 0 ? await page.evaluate((el) => el.textContent, nameElement[0]) : '';
     const brandText = brandElement.length > 0 ? await page.evaluate((el) => el.textContent, brandElement[0]) : '';
-    const categoryText = categoryElemt.length > 0 ? await page.evaluate((el) => el.textContent, categoryElemt[0]) : '';
+    const categoryText = categoryElement.length > 0 ? await page.evaluate((el) => el.textContent, categoryElement[0]) : '';
 
-    if (nameElement.length > 0) {
-      const listXPath = '/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/ul';
-      const listData = await page.evaluate((listXPath) => {
-        const list = document.evaluate(listXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        const data = {};
-
-        if (list) {
-          const items = list.getElementsByTagName('li');
-          for (const item of items) {
-            const parts = item.textContent.split(':');
-            if (parts.length === 2) {
-              const key = parts[0].trim();
-              const value = parts[1].trim();
-              data[key] = value;
-            }
-          }
-        }
-
-        return data;
-      }, listXPath);
-
-      // Create a single string with the formatted data
-      const formattedListData = Object.keys(listData)
-        .map((key) => `${key}: ${listData[key]}`)
-        .join('\n');
-
-      const [imageElement] = await page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div[1]/div[1]/a/figure/img');
-
-      if (imageElement) {
-        const imageUrl = await imageElement.evaluate((img) => img.src);
-        const response = await fetch(imageUrl);
-
-        if (response.ok && uuid1) {
-          const buffer = await response.buffer();
-          const localFilename = `${uuid1}.jpg`;
-
-          // Upload the image to Minio
-          const bucketName = 'vardast'; // Replace with your Minio bucket name
-          const objectName = localFilename;
-
-          try {
-            await minioClient.putObject(bucketName, objectName, buffer, buffer.length);
-            console.log(`Image uploaded to Minio: ${objectName}`);
-          } catch (error) {
-            console.error(`Failed to upload image to Minio: ${error}`);
-          }
-        } else {
-          console.error(`Failed to download image: ${imageUrl}`);
-        }
-      } else {
-        console.log('No imageElement found on the page.');
-      }
-
+    if (pageUrl) {
+      console.log('URL:', pageUrl);
       if (nameText.trim() !== '') {
-        console.log('NAME:', nameText.trim(), 'PRICE:', priceText.trim(), 'URL:', pageUrl);
-        await pool.query('INSERT INTO scraped_data(name, url, price, brand, SKU,description,category) VALUES($1, $2, $3, $4, $5,$6,$7)',
+        console.log('NAME:', nameText.trim(), 'PRICE:', priceText.trim());
+        await pool.query('INSERT INTO scraped_data(name, url, price, brand, SKU, description, category) VALUES($1, $2, $3, $4, $5, $6, $7)',
           [nameText.trim(), pageUrl, priceText.trim() ?? 0, brandText.trim() ?? '', uuid1, formattedListData, categoryText.trim() ?? '']);
       }
-    }
-
-    const hrefs = await page.evaluate(() => {
-      const links = Array.from(document.querySelectorAll('a'));
-      return links.map((link) => link.getAttribute('href'));
-    });
-
-    for (const href of hrefs) {
-      try {
-        if (href) {
-          let outputUrl;
-          if (!href.startsWith('https://')) {
-            outputUrl = initialPage + href;
-          } else {
-            outputUrl = href;
-          }
-          if (outputUrl && outputUrl.startsWith(startUrlPattern2)) {
-            const result = await pool.query('SELECT * FROM unvisited WHERE url = $1', [outputUrl]);
-            if (result.rows.length === 0) {
-              await pool.query('INSERT INTO unvisited(url) VALUES($1)', [outputUrl]);
-            }
-          }
-        }
-      } catch (error) {
-        // console.error(error);
-      }
+    } else {
+      console.error('No valid URL found on the page.');
     }
   } catch (error) {
     console.error(error);
+  } finally {
+    if (page) {
+      await page.close();
+    }
   }
 }
 
@@ -163,13 +91,12 @@ async function main() {
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
       ],
-    },
-  });
+    });
 
   cluster.queue(initialPage, (page) => processPage(page));
 
   cluster.on('taskerror', (err, data) => {
-    console.log(`Error crawling ${data}: ${err.message}`);
+    console.error(`Error crawling ${data}: ${err.message}`);
   });
 
   await createBrowser();
