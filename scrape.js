c05562d991260f73ae395dbaf37f707adf17ba62
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-10-19 20:13:54 +0330

    c

--- scrape.js
+++ scrape.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const { Client } = require('pg');
 const cron = require('node-cron');
 const fetch = require('node-fetch');
