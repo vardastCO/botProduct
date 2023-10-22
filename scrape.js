f3a92cb0b3e3c8e2e00d6b405b9c3ab698270dbe
partovi <partovi74@gmail.com>
2023-10-22 06:08:35 -0700

    change seed pattern

--- scrape.js
+++ scrape.js
@@ -1,10 +1,11 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 const { Client } = require('pg');
 const cron = require('node-cron');
 const fetch = require('node-fetch');
+const fs = require('fs-extra');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 const Minio = require('minio');
-const genericPool = require('generic-pool');
 const minioClient = new Minio.Client({
   endPoint: 'storage', // Use the service name defined in your Docker Compose file
   port: 9000,
@@ -13,216 +14,191 @@ const minioClient = new Minio.Client({
   secretKey: 'str0ngP@ss', // Use the secret key defined in your Docker Compose file
 });
 
-
 const pool = new Client({
   user: 'db',
   host: 'postgres', // Use the service name defined in docker-compose.yml
   database: 'mydb', // This should match the POSTGRES_DB in docker-compose.yml
   password: 'root',
   port: 5432,
-  max: 25, // A
 });
 
 let browser;
 
-const browserFactory = {
-  create: async () => {
-    const browser = await puppeteer.launch({
+async function createBrowser() {
+  try {
+    browser = await puppeteer.launch({
       headless: true,
-      executablePath: '/usr/bin/google-chrome-stable',
+      executablePath: process.env.NODE_ENV === "production" ?
+        process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--disable-extensions',
-        '--disable-gpu'
       ],
     });
-    return browser;
-  },
-  destroy: (browser) => {
-    browser.close();
-  }
-};
-
-const browserPool = genericPool.createPool(browserFactory, {
-  max: 10, // Maximum number of browsers in the pool
-  min: 2,  // Minimum number of browsers to keep in the pool
-});
-
-async function acquireBrowser() {
-  return browserPool.acquire();
-}
-
-async function releaseBrowser(browser) {
-  browserPool.release(browser);
-}
-
-async function createBrowser() {
-  try {
-    browser = await acquireBrowser();
     return browser;
   } catch (error) {
     throw error;
   }
 }
+const startUrlPattern2 = 'https://www.tileiran.co/fa/';
+const initialPage = 'https://www.tileiran.co/fa/%D9%81%D8%B1%D9%88%D8%B4%DA%AF%D8%A7%D9%87-%D8%A2%D9%86%D9%84%D8%A7%DB%8C%D9%86.html';
 
-const initialPage = 'https://kashiland.com/store';
-const startUrlPattern2 = 'https://kashiland.com/store/prod'
 async function processPage(pageUrl) {
+  
+  const page = await browser.newPage();
+  await page.goto(pageUrl, { timeout: 350000 });
 
-      const browser = await createBrowser();
-      const page = await browser.newPage();
+  try {
+    const uuid1 = uuidv4();
+    const [priceElement, nameElement, brandElement] = await Promise.all([
+      page.$x('/html/body/div[2]/section[3]/div/div/div/main/div[1]/div/div/div/div/div/div/form/div/div[4]/div[1]/span/span/span[1]'),
+      page.$x('/html/body/div[2]/section[3]/div/div/div/main/div[1]/div/div/div/div/div/div/form/div/div[3]/div[1]/div[1]/a'),
+      page.$x('/html/body/div[2]/section[3]/div/div/div/main/div[1]/div/div/div/div/div/div/form/div/div[3]/div[1]/div[3]/a'),
+    ]);
 
-      try {
-        await page.goto(pageUrl, { timeout: 120000 });
-        const uuid1 = uuidv4();
-        const [priceElement, nameElement, brandElement,categoryElemt] = await Promise.all([
-          page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[1]/span[2]'),
-          page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[1]/div/div/h1'),
-          page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/ul/li[2]/a'),
-          page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a'),
-        
-        ]);
-        const priceText = priceElement.length > 0 ? await page.evaluate((el) => el.textContent, priceElement[0]) : '';
-        const nameText = nameElement.length > 0 ? await page.evaluate((el) => el.textContent, nameElement[0]) : '';
-        const brandText = brandElement.length > 0 ? await page.evaluate((el) => el.textContent, brandElement[0]) : '';
-        const categoryText = categoryElemt.length > 0 ? await page.evaluate((el) => el.textContent, categoryElemt[0]) : '';
+    if (nameElement.length > 0) {
+      const [priceText, nameText, brandText] = await Promise.all([
+        page.evaluate((el) => el.textContent, priceElement[0]),
+        page.evaluate((el) => el.textContent, nameElement[0]),
+        page.evaluate((el) => el.textContent, brandElement[0]),
+      ]);
 
+      const tableXPath = '/html/body/div[2]/section[3]/div/div/div/main/div[1]/div/div/div/div/div/div/div[3]/div/div[2]/div[2]/table';
 
-        if (nameElement.length > 0) {
-          const listXPath = '/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div/ul';
+      const tableData = await page.evaluate((tableXPath) => {
+        const table = document.evaluate(tableXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        const data = {};
 
-          const listData = await page.evaluate((listXPath) => {
-            const list = document.evaluate(listXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-            const data = {};
-          
-            if (list) {
-              const items = list.getElementsByTagName('li');
-              for (const item of items) {
-                const parts = item.textContent.split(':');
-                if (parts.length === 2) {
-                  const key = parts[0].trim();
-                  const value = parts[1].trim();
-                  data[key] = value;
-                }
-              }
+        if (table) {
+          const rows = table.getElementsByTagName('tr');
+          for (const row of rows) {
+            const cells = row.getElementsByTagName('td');
+            if (cells.length === 2) {
+              const key = cells[0].textContent.trim();
+              const value = cells[1].textContent.trim();
+              data[key] = value;
             }
-          
-            return data;
-          }, listXPath);
-          
-          // Create a single string with the formatted data
-          const formattedListData = Object.keys(listData)
-            .map((key) => `${key}: ${listData[key]}`)
-            .join('\n');
-
-          const [imageElement] = await page.$x('/html/body/form/div[3]/div/section/div[7]/div/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div[1]/div[1]/a/figure/img');
-
-          if (imageElement) {
-            const imageUrl = await imageElement.evaluate((img) => img.src);
-            const response = await fetch(imageUrl);
-
-            if (response.ok && uuid1) {
-              const buffer = await response.buffer();
-              const localFilename = `${uuid1}.jpg`;
-          
-              // Upload the image to Minio
-              const bucketName = 'vardast'; // Replace with your Minio bucket name
-              const objectName = localFilename;
-          
-              try {
-                await minioClient.putObject(bucketName, objectName, buffer, buffer.length);
-                console.log(`Image uploaded to Minio: ${objectName}`);
-              } catch (error) {
-                console.error(`Failed to upload image to Minio: ${error}`);
-              }
-            } else {
-              console.error(`Failed to download image: ${imageUrl}`);
-            }
-          } else {
-            console.log('No imageElement found on the page.');
-          }
-          if (nameText.trim() !== '') {
-            console.log('NAME:', nameText.trim(), 'PRICE:', priceText.trim(), 'URL:', pageUrl);
-            await pool.query('INSERT INTO scraped_data(name, url, price, brand, SKU,description,category) VALUES($1, $2, $3, $4, $5,$6,$7)',
-              [nameText.trim(), pageUrl, priceText.trim() ?? 0, brandText.trim() ?? '', uuid1,
-            formattedListData,categoryText.trim() ?? '']);
           }
         }
+        return data;
+      }, tableXPath);
 
-        const hrefs = await page.evaluate(() => {
-          const links = Array.from(document.querySelectorAll('a'));
-          return links.map((link) => link.getAttribute('href'));
-        });
-        for (const href of hrefs) {
+      // Create a single string with the formatted data
+      const formattedTableData = Object.keys(tableData)
+        .map((key) => `${key}: ${tableData[key]}`)
+        .join('\n');
+
+
+      const [imageElement] = await page.$x('/html/body/div[2]/section[3]/div/div/div/main/div[1]/div/div/div/div/div/div/form/div/div[2]/div[1]/div[1]/div/div/div/a/img');
+
+      if (imageElement) {
+        const imageUrl = await imageElement.evaluate((img) => img.src);
+        const response = await fetch(imageUrl);
+
+        if (response.ok && uuid1) {
+          const buffer = await response.buffer();
+          const localFilename = `${uuid1}.jpg`;
+
+          // Upload the image to Minio
+          const bucketName = 'vardast'; // Replace with your Minio bucket name
+          const objectName = localFilename;
+
           try {
-            if (href) { // Check if href is not null
-              if (!href.startsWith('https://')) {
-                var outputUrl = initialPage + href;
-              } else {
-                var outputUrl = href;
-              }
-              if (outputUrl && outputUrl.startsWith(startUrlPattern2)) {
-                const result = await pool.query('SELECT * FROM unvisited WHERE url = $1', [outputUrl]);
-                if (result.rows.length === 0) {
-                  await pool.query('INSERT INTO unvisited(url) VALUES($1)', [outputUrl]);
-                }
-              }
-            }
+             await minioClient.putObject(bucketName, objectName, buffer, buffer.length);
+            console.log(`Image uploaded to Minio: ${objectName}`);
           } catch (error) {
-            console.error(error);
+            console.error(`Failed to upload image to Minio: ${error}`);
+          }
+        } else {
+          console.error(`Failed to download image: ${imageUrl}`);
+        }
+      } else {
+        console.log('No imageElement found on the page.');
+      }
+
+      if (nameText.trim() !== '') {
+        console.log('NAME:', nameText.trim(), 'PRICE:', priceText.trim(), 'URL:', pageUrl);
+        await pool.query('INSERT INTO scraped_data(name, url, price, brand, SKU,description) VALUES($1, $2, $3, $4, $5,$6)',
+          [nameText.trim(), pageUrl, priceText.trim() ?? 0, brandText.trim() ?? '', uuid1,
+        formattedTableData]);
+      }
+    }
+
+    const hrefs = await page.evaluate(() => {
+      const links = Array.from(document.querySelectorAll('a'));
+      return links.map((link) => link.getAttribute('href'));
+    });
+    for (const href of hrefs) {
+      try {
+        if (href) { // Check if href is not null
+          if (!href.startsWith('https://')) {
+            var outputUrl = initialPage + href;
+          } else {
+            var outputUrl = href;
+          }
+          if (outputUrl && outputUrl.startsWith(startUrlPattern2)) {
+            const result = await pool.query('SELECT * FROM unvisited WHERE url = $1', [outputUrl]);
+            if (result.rows.length === 0) {
+              await pool.query('INSERT INTO unvisited(url) VALUES($1)', [outputUrl]);
+            }
           }
         }
       } catch (error) {
         console.error(error);
-      } finally {
-        if (page) {
-          await page.close();
-        }
-        releaseBrowser(browser); 
       }
-  
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await page.close();
+  }
 }
 
 async function main() {
   try {
     await createBrowser();
-    await pool.connect(); 
-    await pool.query('INSERT INTO unvisited(url) VALUES($1)', [initialPage]);
+    await pool.connect();
 
     cron.schedule('* * * * *', async () => {
       try {
-        // Get the next unvisited URL
-        const result = await pool.query('SELECT url FROM unvisited WHERE url NOT IN (SELECT url FROM visited) LIMIT 1');
-    
-        if (result.rows.length > 0) {
-          const url = result.rows[0].url;
-          
-          // Delete the URL from unvisited
-          await pool.query('DELETE FROM unvisited WHERE url = $1', [url]);
+        let currentHref = await pool.query('SELECT url FROM unvisited LIMIT 1');
+        let visitedCount = 0;
 
-          console.log('url',url)
-    
-          // Process the URL if it's not in the "visited" table
-          const visitedCheckResult = await pool.query('SELECT COUNT(*) FROM visited WHERE url = $1', [url]);
-          const visitedCount = visitedCheckResult.rows[0].count;
+        if (currentHref.rows.length > 0) {
+          const visitedCheckResult = await pool.query('SELECT COUNT(*) FROM visited WHERE url = $1', [currentHref.rows[0].url]);
+          visitedCount = visitedCheckResult.rows[0].count;
+          currentHref = currentHref.rows[0].url;
+        } else {
+          currentHref = initialPage;
+        }
 
-          console.log('visitedCount',visitedCount)
-    
-          if (visitedCount == 0) {
-            // Attempt to process the URL
+        if (visitedCount == 0) {
+          await pool.query('DELETE FROM unvisited WHERE url = $1', [currentHref]);
+          await pool.query('INSERT INTO visited(url) VALUES($1)', [currentHref]);
+
+          const pageForEvaluation = await browser.newPage();
+          let retryCount = 0;
+          const maxRetries = 10000;
+
+          while (retryCount < maxRetries) {
             try {
-              console.log('url2222',url)
-              await processPage(url);
-    
-              // Insert it into visited
-              await pool.query('INSERT INTO visited(url) VALUES($1)', [url]);
+              await processPage(currentHref);
+              break;
             } catch (error) {
-              console.error(`Failed to process URL: ${url}`);
-              console.error(error);
+              if (error.name === 'TimeoutError') {
+                retryCount++;
+              }
             }
           }
+
+          if (retryCount >= maxRetries) {
+            await pageForEvaluation.close();
+          }
+
+          await pageForEvaluation.close();
+        } else {
+          await pool.query('DELETE FROM unvisited WHERE url = $1', [currentHref]);
         }
       } catch (error) {
         console.error(error);
