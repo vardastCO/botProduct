f9f95d26d063965b9aac160beb91e698c3480a4f
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-10-27 17:03:54 +0330

    init

--- container_memory_monitor.py
+++ container_memory_monitor.py
@@ -4,17 +4,29 @@ import psutil
 # Initialize Docker client
 client = docker.from_env()
 
-# Define the memory threshold in bytes (e.g., 8GB)
+# Define the memory threshold in bytes (e.g., 9GB)
 memory_threshold = 9 * 1024 * 1024 * 1024
 
-# Get a list of all running containers
-containers = client.containers.list()
+# Get a list of all Docker images
+images = client.images.list()
 
-# Iterate through containers and check memory usage
-for container in containers:
-    stats = container.stats(stream=False)
-    memory_usage = stats['memory_stats']['usage']
+# Calculate the total memory usage of all images
+total_memory_usage = 0
 
-    if memory_usage > memory_threshold:
-        # Container memory usage exceeds the threshold, restart it
+for image in images:
+    image_id = image.id
+    image_info = client.images.get(image_id)
+    image_size = image_info.attrs['Size']
+
+    # Convert image size to bytes (remove 'B' from the size string)
+    image_size_in_bytes = int(image_size[:-1])
+
+    total_memory_usage += image_size_in_bytes
+
+# Print the total memory usage of all images
+print(f"Total Memory Usage of All Images: {total_memory_usage} bytes")
+
+if total_memory_usage > memory_threshold:
+    # Total memory usage of all images exceeds the threshold, restart them
+    for container in client.containers.list():
         container.restart()
