71d13561dd918591b9d3d3e192f975075618b1cd
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-10-27 22:52:22 +0330

    init

--- scrape.js
+++ scrape.js
@@ -165,7 +165,7 @@ async function main() {
 
     // await pool.query('INSERT INTO unvisited(url) VALUES($1)', [initialPage]);
 
-    cron.schedule('*/3 * * * *', async () => {
+    cron.schedule('*/5 * * * *', async () => {
       try {
         let currentHref = await pool.query('SELECT url FROM unvisited ORDER BY RANDOM() LIMIT 1');
 
