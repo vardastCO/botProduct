67a265bab12dad404244911f524779727c3699b0
farbodnp74 <91656406+farbodnp74@users.noreply.github.com>
2023-10-28 22:23:28 +0330

    init

--- py/script.py
+++ py/script.py
@@ -1,76 +1,72 @@
-# from telegram import Bot
-# from telegram.error import TelegramError
-# import asyncio
-# import time
-# import psutil
-# import os  # Import the os module
-# import subprocess
-# import docker 
+from telegram import Bot
+from telegram.error import TelegramError
+import asyncio
+import time
+import psutil
+import os  # Import the os module
 
-# # Telegram Bot Token
-# bot_token = '6918624503:AAFSU4bwTBmAa2w2T7ElJ9fY4XlUA6MaQ4Q'
+# Telegram Bot Token
+bot_token = '6918624503:AAFSU4bwTBmAa2w2T7ElJ9fY4XlUA6MaQ4Q'
 
-# # Chat ID (can be a group or your user ID)
-# chat_id = '1839030'  # Replace with your actual chat ID
+# Chat ID (can be a group or your user ID)
+chat_id = '1839030'  # Replace with your actual chat ID
 
-# async def send_message(message):
-#     bot = Bot(token=bot_token)
-#     try:
-#         await bot.send_message(chat_id=chat_id, text=message)
-#     except TelegramError as e:
-#         # Handle the Telegram error, or you can log it
-#         print(f"Failed to send message: {e}")
+async def send_message(message):
+    bot = Bot(token=bot_token)
+    try:
+        await bot.send_message(chat_id=chat_id, text=message)
+    except TelegramError as e:
+        # Handle the Telegram error, or you can log it
+        print(f"Failed to send message: {e}")
 
-# def get_ram_usage():
-#     mem = psutil.virtual_memory()
-#     return mem.percent
+def get_ram_usage():
+    mem = psutil.virtual_memory()
+    return mem.percent
 
-# # def restart_docker_containers():
-# #     try:
-# #         client = docker.from_env()
-# #         containers = client.containers.list()
-# #         for container in containers:
-# #             container.restart()
-# #         print("All Docker containers have been restarted.")
-# #     except Exception as e:
-# #         print(f"Failed to restart Docker containers: {e}")
+def restart_docker_containers():
+    try:
+        # Run the "docker restart" command for all running containers
+        os.system('docker ps -q | xargs -I % docker restart %')
+        print("All Docker containers have been restarted.")
+    except Exception as e:
+        print(f"Failed to restart Docker containers: {e}")
 
-# async def send_memory_usage():
-#     ram_usage = get_ram_usage()
-#     message = f"Memory Usage is {ram_usage}%."
-#     await send_message(message)
+async def send_memory_usage():
+    ram_usage = get_ram_usage()
+    message = f"Memory Usage is {ram_usage}%."
+    await send_message(message)
 
-# if __name__ == "__main__":
-#     max_retries = 3  # Adjust the maximum number of retry attempts
-#     retry_delay = 10  # Adjust the delay (in seconds) between retries
-#     retry_count = 0
+if __name__ == "__main__":
+    max_retries = 3  # Adjust the maximum number of retry attempts
+    retry_delay = 10  # Adjust the delay (in seconds) between retries
+    retry_count = 0
 
-#     while retry_count < max_retries:
-#         ram_threshold = 70  # Adjust the threshold as needed
-#         ram_usage = get_ram_usage()
+    while retry_count < max_retries:
+        ram_threshold = 50  # Adjust the threshold as needed
+        ram_usage = get_ram_usage()
 
-#         try:
-#             # Your code to monitor RAM usage goes here
-#             # Check if ram_usage >= ram_threshold
+        try:
+            # Your code to monitor RAM usage goes here
+            # Check if ram_usage >= ram_threshold
 
-#             if ram_usage >= ram_threshold:
-#                 message = f"High RAM usage alert! RAM usage is {ram_usage}%."
-#                 asyncio.run(send_message(message))
-#                 # restart_docker_containers()
-#                 break  # Alert sent successfully, exit the loop
+            if ram_usage >= ram_threshold:
+                message = f"High RAM usage alert! RAM usage is {ram_usage}%."
+                asyncio.run(send_message(message))
+                # restart_docker_containers()
+                break  # Alert sent successfully, exit the loop
 
-#         except Exception as e:
-#             # Handle exceptions and log the error
-#             print(f"An error occurred: {str(e)}")
-#             # You can also log the error to a file if needed
+        except Exception as e:
+            # Handle exceptions and log the error
+            print(f"An error occurred: {str(e)}")
+            # You can also log the error to a file if needed
 
-#         retry_count += 1
-#         if retry_count < max_retries:
-#             # Wait for the specified delay before retrying
-#             time.sleep(retry_delay)
+        retry_count += 1
+        if retry_count < max_retries:
+            # Wait for the specified delay before retrying
+            time.sleep(retry_delay)
 
-#     # After the memory check loop, you can call the memory usage function
-#     asyncio.run(send_memory_usage())
+    # After the memory check loop, you can call the memory usage function
+    asyncio.run(send_memory_usage())
 import os
 
 def list_running_containers():
