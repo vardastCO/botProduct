00c78d2f096b9c45645bdfe50e5ad3c61d7db139
partovi <partovi74@gmail.com>
2023-10-28 07:12:10 -0700

    cahnge filter admin pagination

--- py/script.py
+++ py/script.py
@@ -1,14 +1,14 @@
-import psutil  # For memory monitoring
 from telegram import Bot
 from telegram.error import TelegramError
 import asyncio
 import time
-import subprocess  # Import the subprocess module
+import psutil
 
+# Telegram Bot Token
 bot_token = '6918624503:AAFSU4bwTBmAa2w2T7ElJ9fY4XlUA6MaQ4Q'
 
 # Chat ID (can be a group or your user ID)
-chat_id = '1839030' 
+chat_id = '1839030'  # Replace with your actual chat ID
 
 async def send_message(message):
     bot = Bot(token=bot_token)
@@ -27,7 +27,7 @@ async def send_memory_usage():
     message = f"Memory Usage is {ram_usage}%."
     await send_message(message)
 
-if __name__ == "__main":
+if __name__ == "__main__":
     max_retries = 3  # Adjust the maximum number of retry attempts
     retry_delay = 10  # Adjust the delay (in seconds) between retries
     retry_count = 0
@@ -39,14 +39,7 @@ if __name__ == "__main":
         if ram_usage >= ram_threshold:
             message = f"High RAM usage alert! RAM usage is {ram_usage}%."
             asyncio.run(send_message(message))
-            
-            # Restart all Docker containers
-            try:
-                subprocess.run(["docker", "restart", "$(docker ps -q)"])
-            except subprocess.CalledProcessError as e:
-                print(f"Failed to restart Docker containers: {e}")
-            
-            break  # Alert sent successfully, Docker containers restarted, exit the loop
+            break  # Alert sent successfully, exit the loop
 
         retry_count += 1
         if retry_count < max_retries:
