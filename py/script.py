db174f1d040ccd9db60ca9a9a2d2ea2e35dd4405
partovi <partovi74@gmail.com>
2023-10-28 06:14:04 -0700

    cahnge filter admin pagination

--- py/script.py
+++ py/script.py
@@ -1,29 +1,30 @@
 import psutil  # For memory monitoring
 from telegram import Bot
 from telegram.error import TelegramError
+import asyncio
 import time
 
 # Telegram Bot Token
 bot_token = '6918624503:AAFSU4bwTBmAa2w2T7ElJ9fY4XlUA6MaQ4Q'
 
 # Chat ID (can be a group or your user ID)
-chat_id = '1839030' # Replace with your actual chat ID# Replace with your actual chat ID
+chat_id = '1839030'   # Replace with your actual chat ID
+
+async def send_message(message):
+    bot = Bot(token=bot_token)
+    try:
+        await bot.send_message(chat_id=chat_id, text=message)
+    except TelegramError as e:
+        # Handle the Telegram error, or you can log it
+        print(f"Failed to send message: {e}")
 
 def get_ram_usage():
     mem = psutil.virtual_memory()
     return mem.percent
 
-def send_message(message):
-    bot = Bot(token=bot_token)
-    try:
-        bot.send_message(chat_id=chat_id, text=message)
-    except TelegramError as e:
-        # Handle the Telegram error, or you can log it
-        print(f"Failed to send message: {e}")
-
-def send_test_message():
+async def send_test_message():
     test_message = "This is a test message."
-    send_message(test_message)
+    await send_message(test_message)
 
 if __name__ == "__main__":
     max_retries = 3  # Adjust the maximum number of retry attempts
@@ -36,7 +37,7 @@ if __name__ == "__main__":
 
         if ram_usage >= ram_threshold:
             message = f"High RAM usage alert! RAM usage is {ram_usage}%."
-            send_message(message)
+            asyncio.run(send_message(message))
             break  # Alert sent successfully, exit the loop
 
         retry_count += 1
@@ -45,4 +46,4 @@ if __name__ == "__main__":
             time.sleep(retry_delay)
 
     # After the memory check loop, you can call the test message function
-    send_test_message()
+    asyncio.run(send_test_message())
