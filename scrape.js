e93f284242a89357c682364e38c5d9039abbdc36
partovi <partovi74@gmail.com>
2023-10-28 02:44:51 -0700

    cahnge filter admin pagination

--- scrape.js
+++ scrape.js
@@ -53,7 +53,7 @@ const initialPage = 'https://www.tileiran.co/fa/%D9%81%D8%B1%D9%88%D8%B4%DA%AF%D
 async function processPage(pageUrl) {
   
   const page = await browser.newPage();
-  await page.goto(pageUrl+ '?filter_نمایش_کالاهای_موجود_54=in_stock', { timeout: 90000 });
+  await page.goto(pageUrl+ '?filter_نمایش_کالاهای_موجود_54=in_stock', { timeout: 250000 });
   try {
     const uuid1 = uuidv4();
     if (pageUrl.includes('product')){
@@ -168,7 +168,7 @@ async function main() {
     // await pool.query('INSERT INTO unvisited(url) VALUES($1)', [initialPage]);
     await createBrowser();
     await pool.connect();
-    cron.schedule('*/5 * * * *', async () => {
+    cron.schedule('*/10 * * * *', async () => {
       try {
     
         const freeMemoryGB = os.freemem() / (1024 * 1024 * 1024);
@@ -194,7 +194,7 @@ async function main() {
             await pool.query('DELETE FROM unvisited WHERE url = $1', [currentHref]);
             await pool.query('INSERT INTO visited(url) VALUES($1)', [currentHref]);
   
-            const randomDelay = Math.floor(Math.random() * 90000); // 0 to 50 seconds
+            const randomDelay = Math.floor(Math.random() * 240000); // 0 to 50 seconds
             await new Promise((resolve) => setTimeout(resolve, randomDelay));
   
         
