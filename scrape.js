bc957e8497b17df7918af954ff6a4a5eaa098efd
partovi <partovi74@gmail.com>
2023-11-05 04:42:19 -0800

    v

--- scrape.js
+++ scrape.js
@@ -183,7 +183,7 @@ async function main() {
 
     await createBrowser();
     await pool.connect();
-        await pool.query('INSERT INTO unvisited(url) VALUES($1)', [initialPage]);
+        // await pool.query('INSERT INTO unvisited(url) VALUES($1)', [initialPage]);
     cron.schedule('*/4 * * * *', async () => {
       try {
    
