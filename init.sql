c803f0b5bc20b55e22201097b2f439264adfa01f
partovi <partovi74@gmail.com>
2023-11-15 03:20:39 -0800

    c

--- init.sql
+++ init.sql
@@ -1,24 +1,7 @@
-CREATE TABLE visited (
-    id SERIAL PRIMARY KEY,
-    url TEXT UNIQUE
-);
-
-CREATE TABLE unvisited (
-    id SERIAL PRIMARY KEY,
-    url TEXT UNIQUE
-);
-
-CREATE TABLE scraped_data (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-);
-
-
 CREATE TABLE bot_price (
     id SERIAL PRIMARY KEY,
-    selLer_id INT,
-    product_id INT,
+    selLerid INT,
+    productid INT,
     url TEXT,
-    price_xpath TEXT,
-
+    pricexpath TEXT,
 );
